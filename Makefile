d3fa01a0a28c98a75425984335d51fd26b273aa8
Aaron Cirillo <amcirillo@valassis.com>
2018-07-29 12:35:03 -0400

    Finished re-arranging files

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	emcc --bind -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s ALLOW_MEMORY_GROWTH=1 -o bin/index.html src/main.c --preload-file resources --use-preload-plugins
+	emcc --bind -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s ALLOW_MEMORY_GROWTH=1 -o bin/index.html src/main.c src/main_loop/main_loop.c --preload-file resources --use-preload-plugins
--- src/main_loop/main_loop.h
+++ src/main_loop/main_loop.h
@@ -1,7 +1,10 @@
+#include "../main.h"
+
 #ifndef MAIN_LOOP_H
 #define MAIN_LOOP_H
 
-int getXCoord(context *ctx, SDL_Rect *r);
-int getYCoord(context *ctx, SDL_Rect *r);
+void EMSCRIPTEN_KEEPALIVE mainloop(void *arg);
+int EMSCRIPTEN_KEEPALIVE getXCoord(context *ctx, SDL_Rect *r);
+int EMSCRIPTEN_KEEPALIVE getYCoord(context *ctx, SDL_Rect *r);
 
 #endif
